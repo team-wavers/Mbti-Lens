3a9193028d9daf359fd2c4c1e22ea7395f4518e3
woomae <cwg0809@naver.com>
2023-08-13 01:32:13 +0900

    Mbti 설정기능 생성
    - mbti 설정을 위해 user테이블의 email을 참조해 생성
    - 추후 코멘트 기능을 위해 엔티티 수정이 필요

--- server/src/api/v1/v1.module.ts
+++ server/src/api/v1/v1.module.ts
@@ -3,10 +3,11 @@ import { V1Service } from './v1.service';
 import { V1Controller } from './v1.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { MbtiModule } from './mbti/mbti.module';
 
 @Module({
   controllers: [V1Controller],
   providers: [V1Service],
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, MbtiModule],
 })
 export class V1Module {}
