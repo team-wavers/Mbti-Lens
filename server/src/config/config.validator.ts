4d6577696a6e8d5ab9714bd853db4d6608eed6c3
다운 <j_d_woon@naver.com>
2023-09-06 17:21:46 +0900

    refactor: env 모드 설정
    - prod, dev, local, test 4가지 환경으로 구성
    - .env.prod*, .env.dev*, .env.test*, .env.local로 .env 생성하여 사용하도록 수정

--- server/src/config/config.validator.ts
+++ server/src/config/config.validator.ts
@@ -1,5 +1,45 @@
 import { ConfigModuleOptions } from '@nestjs/config';
 import * as Joi from 'joi';
+import * as glob from 'glob';
+import * as path from 'path';
+
+const getEnvFilePath = () => {
+  const nodeEnv = process.env.NODE_ENV?.toLowerCase();
+
+  if (!nodeEnv) {
+    throw new Error(
+      'NODE_ENV is undefined. Please set the NODE_ENV environment variable.',
+    );
+  }
+
+  const envModes = ['dev', 'prod', 'test', 'local'];
+  const mode = envModes.find((mod) => nodeEnv.startsWith(mod));
+
+  if (!mode) {
+    throw new Error(`Invalid NODE_ENV value: ${nodeEnv}`);
+  }
+
+  const currentDir = process.cwd();
+  let envFile: string | null = null;
+
+  glob.sync(`.env.${mode}*`, { cwd: currentDir }).forEach((file) => {
+    if (!envFile) {
+      envFile = file;
+    }
+  });
+
+  if (envFile) {
+    if (!glob.sync(path.resolve(currentDir, envFile)).length) {
+      throw new Error(`No environment file found for NODE_ENV: ${nodeEnv}`);
+    }
+  } else {
+    throw new Error(`No file starting with .env.${mode} found.`);
+  }
+
+  return envFile;
+};
+
+const envFilePath = getEnvFilePath();
 
 export const CONFIG_VALIDATOR: ConfigModuleOptions = {
   validationSchema: Joi.object({
@@ -13,5 +53,8 @@ export const CONFIG_VALIDATOR: ConfigModuleOptions = {
   }),
   isGlobal: true,
   cache: true,
-  envFilePath: `.{$process.env.NODE_ENV}.env`,
+  envFilePath: envFilePath,
 };
+
+// dev, develop, development = .env.dev로 실행되게끔
+// prod, production = .env.prod로 실행되게끔
