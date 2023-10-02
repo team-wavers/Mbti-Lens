3703ae8ba7f93fa461692451a6a4eca4fd759e9f
dyslo <auau3@kakao.com>
2023-10-02 19:59:44 +0900

    fix: GeneralResponse 일반화 - data: string으로 설정 후 필요 시 Omit 통해 data 타입 수정

--- front/src/types/response.ts
+++ front/src/types/response.ts
@@ -1,28 +1,14 @@
 export interface GeneralResponse {
     statusCode: number;
     message: string;
+    data: string;
 }
 
-// type APIResponseType = {
-//     SearchResponse: GeneralResponse<{
-//         _id: number;
-//         user_id: number;
-//         ei: "e" | "i" | null;
-//         ns: "n" | "s" | null;
-//         tf: "t" | "f" | null;
-//         pj: "p" | "j" | null;
-//         ei_like: number;
-//         ns_like: number;
-//         tf_like: number;
-//         pj_like: number;
-//     }>;
-//     SubmitResponse: GeneralResponse;
-// };
-
-export interface SearchResponse extends GeneralResponse {
+export interface SearchResponse extends Omit<GeneralResponse, "data"> {
     data: {
         _id: number;
         user_id: number;
+        nickname: string;
         ei: "e" | "i" | " ";
         ns: "n" | "s" | " ";
         tf: "t" | "f" | " ";
