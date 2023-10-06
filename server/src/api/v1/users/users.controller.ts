a8bbfb350501ba03c1c35e978bf097df3bc1b0e3
다운 <j_d_woon@naver.com>
2023-10-06 19:22:17 +0900

    feature: user public_key 조회
    - userId를 통해 해당 유저의 public_key 조회 가능
    - 존재 하지 않는 userId로 조회시 에러메세지 전송

--- server/src/api/v1/users/users.controller.ts
+++ server/src/api/v1/users/users.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { StandardResponseDto } from 'src/dto/standard-response.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('v1/users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get(':userId/public_key')
+  async findPublickey(@Param('userId') paramUserId: number): Promise<any> {
+    const user = await this.usersService.findOne({
+      where: { _id: paramUserId },
+    });
+
+    if (!user) {
+      throw new BadRequestException('user not found');
+    }
+
+    const result = user.public_key;
+
+    return new StandardResponseDto(200, 'api.common.ok', result);
+  }
+}
