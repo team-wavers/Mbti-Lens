9aac358364a72fc27ffee8145b45d8c074195857
dyslo <auau3@kakao.com>
2023-10-09 13:45:36 +0900

    fix: pm2 ecosystem config modify naming - ecosystem config 네이밍 수정: mbti-lens.front -> mbti.lens.front

--- front/ecosystem.config.js
+++ front/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: "mbti-lens.front",
+            name: "mbti.lens.front",
             script: "node_modules/next/dist/bin/next",
             args: "start -p 3000",
             cwd: "./",
