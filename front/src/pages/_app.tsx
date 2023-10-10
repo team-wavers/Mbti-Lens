9e017dabb56b074a83b684bb83da700e1ca83b47
dyslo <auau3@kakao.com>
2023-10-10 18:05:32 +0900

    fix: meta data 수정 - thumbnail 추가 - title, description 수정

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -3,18 +3,15 @@ import GlobalStyle from "@/styles/GlobalStyles";
 import theme from "@/styles/theme";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
-import { RecoilRoot } from "recoil";
 
 const App = ({ Component, pageProps }: AppProps) => {
     return (
-        <RecoilRoot>
-            <ThemeProvider theme={theme}>
-                <GlobalStyle />
-                <CommonLayout>
-                    <Component {...pageProps} />
-                </CommonLayout>
-            </ThemeProvider>
-        </RecoilRoot>
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <CommonLayout>
+                <Component {...pageProps} />
+            </CommonLayout>
+        </ThemeProvider>
     );
 };
 
--- front/src/pages/_document.tsx
+++ front/src/pages/_document.tsx
@@ -39,8 +39,20 @@ export default class MyDocument extends Document {
                         content="width=device-width, initial-scale=1.0"
                     />
                     <meta property="og:type" content="website" />
-                    <meta property="og:site_name" content="website-name" />
-                    <meta property="og:title" content="website-name" />
+                    <meta
+                        property="og:site_name"
+                        content="남이보는 당신의 MBTI는? MBTI Lens"
+                    />
+                    <meta
+                        property="og:title"
+                        content="남이보는 당신의 MBTI는? MBTI Lens"
+                    />
+                    <meta property="og:image" content="./thumbnail.png" />
+                    <meta property="og:image:type" content="image/png" />
+                    <meta
+                        property="og:description"
+                        content="남이 보는 내 MBTI는 어떨까? 쉽게 평가받아 보세요!"
+                    />
                     <link rel="stylesheet" href="../fonts.css" />
                 </Head>
                 <body>
