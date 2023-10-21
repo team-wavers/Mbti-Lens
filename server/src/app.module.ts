600f9e3f063cf0bfdd82549199740c1428c7a7dd
woomae <cwg0809@naver.com>
2023-10-21 16:27:58 +0900

    feature: logging & log-rotate
    - 기존 로거를 nest-winston으로 변경
    - winston에서 제공하는 로그 파일 관리 시스템 적용

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,18 +6,20 @@ import { CONFIG_VALIDATOR } from './config/config.validator';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './common/http-exception.filter';
-import { LoggingMiddleware } from './logger/logging.middleware';
-
+import { Logger } from '@nestjs/common';
+import { LoggerMiddleware } from './logger/logging.middleware';
 @Module({
   imports: [V1Module, ConfigModule.forRoot(CONFIG_VALIDATOR)],
   controllers: [AppController],
   providers: [
     AppService,
     { provide: APP_FILTER, useClass: HttpExceptionFilter },
+    Logger,
   ],
+  exports: [Logger],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggingMiddleware).forRoutes('*'); // 모든 라우트에 대해 미들웨어를 활성화하려면 '*'
+    consumer.apply(LoggerMiddleware).forRoutes('*');
   }
 }
