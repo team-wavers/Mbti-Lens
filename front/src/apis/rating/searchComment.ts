1fdef01ba98a47f8e54bc6cea94ed82bea431db4
dyslo <auau3@kakao.com>
2023-10-29 12:24:23 +0900

    fix: Pagination 구현을 위한 댓글 조회 API 수정 - size, page 등 추가

--- front/src/apis/rating/searchComment.ts
+++ front/src/apis/rating/searchComment.ts
@@ -4,11 +4,19 @@ type Props = {
     userId: number;
     mbti: string;
     public_key: string;
+    page: number;
+    size: number;
 };
 
-const searchComment = async ({ userId, mbti, public_key }: Props) => {
+const searchComment = async ({
+    userId,
+    mbti,
+    public_key,
+    page,
+    size,
+}: Props) => {
     return await instance().get(
-        `/users/${userId}/mbtis/${mbti}/comments?public_key=${public_key}`,
+        `/users/${userId}/mbtis/${mbti}/comments?public_key=${public_key}&page=${page}&size=${size}`,
     );
 };
 
