3be27556949e489e90b221aa92f6b5e738705e2f
dyslo <auau3@kakao.com>
2023-10-31 11:00:24 +0900

    fix: user-scalable 수정

--- front/src/pages/_document.tsx
+++ front/src/pages/_document.tsx
@@ -37,7 +37,7 @@ export default class MyDocument extends Document {
                     <meta charSet="utf-8" />
                     <meta
                         name="viewport"
-                        content="width=device-width, initial-scale=1.0"
+                        content="width=device-width, initial-scale=1.0, user-scalable=0"
                     />
                     <meta property="og:type" content="website" />
                     <meta
